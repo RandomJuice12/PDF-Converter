123325833469696a778757289cccf47201706a3e
RandomJuice12 <RandomJuice121@outlook.com>
2025-11-22 11:58:16 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  // Leave as default; no special config needed
 };
+
+module.exports = nextConfig;
